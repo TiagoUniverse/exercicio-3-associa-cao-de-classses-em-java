70cec466eed12b6d0582b718e829f744402350e9
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-06 14:06:12 -0300

    Atualizações em professor e turma, aonde se criou metodos no main para o cadastro

--- Program.java
+++ Program.java
@@ -3,18 +3,11 @@ import java.util.Scanner;
 public class Program {
    public static void main(String[] args) {
     
-      CadastrarTurma();
+     CadastrarTurma();
 
-    /*
-    Professor Raomi = new Professor();
-    Raomi.nome = "Raomi";
-    Raomi.chapa = "12";
-    Raomi.cpf = "123456789";
-    Raomi.identidade = "1234567";
-    Raomi.sexo = "Masculino";
-    Raomi.turma = turma1.disciplina;
+    CadastrarProfessor();
 
-
+/*
     Aluno Tiago = new Aluno();
     Tiago.cpf = "9876543221";
     Tiago.curso = Raomi.turma;
@@ -26,35 +19,63 @@ public class Program {
     System.out.println("Turma de "+turma1.disciplina+" cadastrada!");
 */
    }
-
    public static void CadastrarTurma(){
-      Scanner sc = new Scanner(System.in);
-  
-      Turma turma1 = new Turma();
-      System.out.println("Bem vindo ao cadastro de turma!");
-      System.out.println("Informe o título da turma: ");
-      turma1.disciplina= sc.nextLine();
-      System.out.println("Informe o bloco da turma: ");
-      turma1.bloco= sc.nextLine();
-      System.out.println("Informe o número da turma: ");
-      turma1.numeroTurma= Integer.parseInt(sc.nextLine()); 
-      System.out.println(" Informe o número da Sala: ");
-      turma1.numeroDaSala= Integer.parseInt(sc.nextLine()); 
-      System.out.println("Por fim, esta turma já está disponível? ");
-      turma1.disponivel = sc.nextBoolean();
+    Scanner sc = new Scanner(System.in);
 
-      System.out.println("Turma "+turma1.disciplina+" cadastrada!");
-      
-  /*
-      Turma turma1 = new Turma();
-  turma1.bloco = 1;
-  turma1.disciplina = "codigo";
-  turma1.disponivel = true;
-  turma1.numeroDaSala = 1;
-  turma1.numeroTurma = 2; 
-  */
+    Turma turma1 = new Turma();
+    System.out.println("Bem vindo ao cadastro de turma!");
+    System.out.println("Informe o título da turma: ");
+    turma1.disciplina= sc.nextLine();
+    System.out.println("Informe o bloco da turma: ");
+    turma1.bloco= sc.nextLine();
+    System.out.println("Informe o número da turma: ");
+    turma1.numeroTurma= Integer.parseInt(sc.nextLine()); 
+    System.out.println(" Informe o número da Sala: ");
+    turma1.numeroDaSala= Integer.parseInt(sc.nextLine()); 
+    System.out.println("Por fim, esta turma já está disponível? ");
+    turma1.disponivel = sc.nextBoolean();
 
-  }
+    System.out.println("Turma "+turma1.disciplina+" cadastrada!");    
+/*
+    Turma turma1 = new Turma();
+turma1.bloco = 1;
+turma1.disciplina = "codigo";
+turma1.disponivel = true;
+turma1.numeroDaSala = 1;
+turma1.numeroTurma = 2; 
+*/
 
-  
+}
+
+public static void CadastrarProfessor(){
+  Scanner sc = new Scanner(System.in);
+
+  Professor prof1 = new Professor();
+  System.out.println("Bem vindo ao cadastro de professores!");
+  System.out.println("Informe o nome do professor: ");
+  prof1.nome= sc.nextLine();
+  System.out.println("Informe a sua chapa: ");
+  prof1.chapa= sc.nextLine();
+  System.out.println("Informe o cpf: ");
+  prof1.cpf= (sc.nextLine()); 
+  System.out.println(" Informe o número da identidade: ");
+  prof1.identidade= (sc.nextLine()); 
+  System.out.println(" Qual o sexo do professor? ");
+  prof1.sexo= (sc.nextLine()); 
+  System.out.println("Por fim, qual é a turma deste professor? ");
+  prof1.turma = sc.nextLine();
+
+  System.out.println("Professor "+prof1.nome+" da turma "+prof1.turma+" cadastrado!");    
+/*
+    Professor Raomi = new Professor();
+    Raomi.nome = "Raomi";
+    Raomi.chapa = "12";
+    Raomi.cpf = "123456789";
+    Raomi.identidade = "1234567";
+    Raomi.sexo = "Masculino";
+    Raomi.turma = turma1.disciplina;
+*/
+}
+
+
 }
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # exercicio-3-associa-cao-de-classses-em-java
 Exercício da faculdade para associar as classes professor, turma e aluno na linguagem Java
 
-Teste de segundo branch
 
 Qualquer coisa pode entrar em contato comigo.
 
